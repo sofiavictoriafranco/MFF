a7caedec93a3d3aa0bfb087c2ca9ad56bb8b1650
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2024-02-08 23:52:04 -0300

    home

--- frontend/frontend/src/components/Home.jsx
+++ frontend/frontend/src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import {useTranslation} from 'react-i18next'
 
 
 
@@ -7,28 +6,26 @@ import {useTranslation} from 'react-i18next'
 
 const Home = () => {
 
-    const [t, i18n] = useTranslation("global")
-
 
     return(
 
-        <section class="home" id="home" data-stellar-background-ratio="0.4">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="st-home-unit">
-						<div class="hero-txt">
-							<p class="hero-work">{t("home.title1")}</p>
-							<h2 class="hero-title">{t("home.title2")}</h2>
-							<a href="#contact" class="btn btn-main btn-lg">{t("home.NavBar-Contact")}</a>
+        <section className="home" id="home" data-stellar-background-ratio="0.4">
+		<div className="container">
+			<div className="row">
+				<div className="col-md-12">
+					<div className="st-home-unit" style={{padding:"258px 0px"}}>
+						<div className="hero-txt">
+							<p className="hero-work">Web Design - Web Development - UX Design</p>
+							<h2 className="hero-title">BEST Template for Agency</h2>
+							<a href="#" className="btn btn-main btn-lg">Find Out More</a>
 						</div>
 
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="mouse-icon">
-			<div class="wheel"></div>
+		<div className="mouse-icon">
+			<div className="wheel"></div>
 		</div>
 	</section>
         
