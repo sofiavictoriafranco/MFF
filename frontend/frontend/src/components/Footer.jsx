7adda39c66601505a0fd41ebc5e4c58e5f77de26
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2024-02-14 16:41:17 -0300

    turnos

---
+++ frontend/frontend/src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+
+const Footer = () => {
+
+    return(
+
+        <div data-spy="scroll" data-target=".main-nav">
+
+     
+    
+        <footer className="site-footer">
+		<div className="container">
+			<div className="row">
+				<div className="col-md-12">
+					&copy; <a >Mental Flex</a> 2023.
+					
+					Email <a href="mailto:info@mental-flex.com">info@mental-flex.com</a>
+					
+				</div>
+				
+			</div>
+		</div>
+	</footer>
+
+        </div>
+    
+
+  
+        
+    )
+}
+
+export default Footer
--- frontend/frontend/src/views/Landing.jsx
+++ frontend/frontend/src/views/Landing.jsx
@@ -9,10 +9,21 @@ import Questions from "../components/Questions"
 import Pricing from "../components/Pricing"
 import Testimonials from "../components/Testimonials/Testimonials"
 import NavBar from "../components/NavBar/NavBar"
+import { Route, Routes, useLocation} from 'react-router-dom';
 
-
-
-
+import Purchase1EN from "../components/Purchase/Purchase1EN";
+import Purchase1ES from "../components/Purchase/Purchase1ES";
+import Purchase2EN from "../components/Purchase/Purchase2EN";
+import Purchase3EN from "../components/Purchase/Purchase3EN";
+import Purchase3ES from "../components/Purchase/Purchase3ES";
+import Purchase4EN from "../components/Purchase/Purchase4EN";
+import Purchase4ES from "../components/Purchase/Purchase4ES";
+import Purchase5EN from "../components/Purchase/Purchase5EN";
+import Purchase5ES from "../components/Purchase/Purchase5ES";
+import Purchase2ES from "../components/Purchase/Purchase2ES";
+import Footer from "../components/Footer"
+import CongratulationsEN from "../components/Congratulations/CongratulationsEN"
+import CongratulationsES from "../components/Congratulations/CongratulationsES"
 
 const Landing = () => {
 
@@ -21,23 +32,49 @@ const Landing = () => {
 
 		<>
 
-    <NavBar/>
+<Routes>
 
+
+  <Route path="/" element={
+    
+<>
+    <NavBar/>
     <Home/>
     <About/>
     <Funfacts/>
     <Services/>
-    <Phrase/>
-    <Questions/>
-    <CallUs/>
+    <Phrase/> 
     <Pricing/>
     <Testimonials/>
+    <Questions/>
+    <CallUs/>
+    <Footer/>
+   
+</>
+  }/>
+
+
+
+                <Route path="/purchase1EN" element={<Purchase1EN/>}/>
+                <Route path="/purchase1ES" element={<Purchase1ES/>}/>
+                <Route path="/purchase2EN" element={<Purchase2EN/>}/>
+                <Route path="/purchase3EN" element={<Purchase3EN/>}/>
+                <Route path="/purchase3ES" element={<Purchase3ES/>}/>
+                <Route path="/purchase4EN" element={<Purchase4EN/>}/>
+                <Route path="/purchase4ES" element={<Purchase4ES/>}/>
+                <Route path="/purchase5EN" element={<Purchase5EN/>}/>
+                <Route path="/purchase5ES" element={<Purchase5ES/>}/>
+                <Route path="/purchase2ES" element={<Purchase2ES/>}/>
+
+
+                <Route path="/congratulationsEN" element={<CongratulationsEN/>}/>
+                <Route path="/congratulationsES" element={<CongratulationsES/>}/>
     
 
 
 
       
-
+</Routes>
   
 
        </>
