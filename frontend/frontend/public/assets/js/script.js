b9c67a8807b54957ed4737ea0710331674c2086b
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2024-02-15 19:50:24 -0300

    scroll

--- frontend/frontend/public/assets/js/script.js
+++ frontend/frontend/public/assets/js/script.js
@@ -5,21 +5,7 @@ $(function () {
 	});
 
 	// Custom Scrollbar
-	var nice = $("html").niceScroll({
-		cursorwidth: 8,
-		cursorborder: "0px solid #fff",
-		cursorborderradius: '0'
-	});
-
-	$('.main-nav a:not(.dropdown-toggle)').bind('click', function(event) {
-		var $anchor = $(this);
-
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1500, 'easeInOutExpo');
-
-		event.preventDefault();
-	});
+	
 	/*
 	* Fun Fact with Count Animation
 	*/
